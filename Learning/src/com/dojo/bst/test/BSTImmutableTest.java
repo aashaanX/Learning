d5d559a21e110792b8267c273350a66d0e19cb4c
arjun <arjun.ep@awpl.co>
2017-01-03 19:10:48 +0530

    test for insert methode added +++
    Not Completed

--- Learning/src/com/dojo/bst/test/BSTImmutableTest.java
+++ Learning/src/com/dojo/bst/test/BSTImmutableTest.java
@@ -1,15 +1,32 @@
 package com.dojo.bst.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
+import com.dojo.bst.BSTImmutable;
+import com.dojo.bst.Node;
+
 public class BSTImmutableTest {
+	
 	@Test
 	public void test(){
 		int x=1;
 		int y=1;
 		assertEquals(2, x+y);
+	}
+	
+	@Test
+	public void testInsertToEmpty(){
+		Node node = new Node(10);
+		BSTImmutable bst = new BSTImmutable(); 
+		assertNotEquals(bst, bst.insert(bst, node));
+		//assertFalse(bst.find(10));
+		//assertNotNull(new_bst);
 	}
 
 }
