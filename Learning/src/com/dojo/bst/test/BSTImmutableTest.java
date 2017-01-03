91d941bdb994018428da456e1e1eb4870dc4808e
arjun <arjun.ep@awpl.co>
2017-01-03 18:11:27 +0530

    Sample Test case to check the working of junit

---
+++ Learning/src/com/dojo/bst/test/BSTImmutableTest.java
@@ -0,0 +1,15 @@
+package com.dojo.bst.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BSTImmutableTest {
+	@Test
+	public void test(){
+		int x=1;
+		int y=1;
+		assertEquals(2, x+y);
+	}
+
+}
